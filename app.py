307d676a5a405912c6b97e9a59e7ad7b74bb4f54
Dennis Mutethia Muga <dennismuga@gmail.com>
2026-07-13 10:54:48 +0300

    layout done

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from datetime import datetime
 import logging
 import os
 
-from flask import abort, Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
-
-from utils import contribute, register, webhooks, withdraw
 
 # Load environment variables
 load_dotenv()
 
 DEBUG_MODE = os.getenv('IS_DEBUG', 'False') in ['True', '1', 't']
+PORT = int(os.getenv('PORT', '5000'))
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -33,16 +32,5 @@ def index():
     return render_template('index.html')
 
 
-
-# @app.route('/terms')
-# def terms_route():
-#     return render_template('terms.html')
-
-
-# @app.route('/privacy')
-# def privacy_route():
-#     return render_template('privacy.html')
-
-
 if __name__ == '__main__':    
-    app.run(debug=DEBUG_MODE)
+    app.run(debug=DEBUG_MODE, host='0.0.0.0', port=PORT)
